64e3d10a1273942895b4ab680027988d53d4008d
ashutosh-basedia <ashutosh.basedia7@gmail.com>
2017-09-13 02:38:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+
 app.get('/article-one', function (req, res) {
   res.send("article one is handled here");
 });
@@ -20,6 +18,10 @@ app.get('/article-two', function (req, res) {
 });
 app.get('/article-third', function (req, res) {
   res.send("article third is handled here");
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
